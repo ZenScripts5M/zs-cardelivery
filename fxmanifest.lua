9f5f033f81f7757f19013bf29d6fa776edab4f12
ZenScripts5M <goodcole0@gmail.com>
2025-05-12 15:19:08 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ZenScripts'
+description 'Car Delivery Job'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
